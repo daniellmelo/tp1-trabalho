d35762a5c2a0996f1d38ab1d99d229eb2715d24c
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-09-22 16:21:32 -0300

    refact-domain-files

--- dominios.hpp
+++ dominios.hpp
@@ -3,41 +3,6 @@
 
 using namespace std;
 
-class Email {
-    private:
-        string value;
-        void validar(string);
-    public:
-        void setEmail(string);
-        string getEmail();
-};
 
-class Texto {
 
-};
 
-class Senha {
-
-};
-
-class Codigo {
-
-};
-
-class Limite {
-    private:
-        int limite;
-        void validar(int);
-    public:
-        void setLimite(string);
-        string getLimite();
-};
-
-class Coluna {
-    private:
-        string coluna;
-        void validar(string);
-    public:
-        void setColuna(string);
-        string getColuna();
-};
