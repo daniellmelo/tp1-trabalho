64fb1dc05c4ba1bf556e9743fe2143b9395388b0
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-09-26 01:34:56 -0300

    Implementação do domínio 'Senha'

--- domains/Senha.cpp
+++ domains/Senha.cpp
@@ -1,2 +1,56 @@
 
 #include "Senha.hpp"
+#include <unordered_set>
+
+
+bool Senha::validar(std::string senha){
+    // Verificar se a senha tem pelo menos 5 caracteres
+    if (senha.size() < 5) {
+        return false;
+    }
+
+    // Verificar se há pelo menos um caractere maiúsculo
+    if (!std::regex_search(senha, std::regex("[A-Z]"))) {
+        return false;
+    }
+
+    // Verificar se há pelo menos um caractere minúsculo
+    if (!std::regex_search(senha, std::regex("[a-z]"))) {
+        return false;
+    }
+
+    // Verificar se há pelo menos um dígito
+    if (!std::regex_search(senha, std::regex("[0-9]"))) {
+        return false;
+    }
+
+    // Verificar se há pelo menos um caractere de pontuação
+    if (!std::regex_search(senha, std::regex("[.,;?!]"))) {
+        return false;
+    }
+
+    // Verificar se não há caracteres duplicados usando um conjunto (unordered_set)
+    std::unordered_set<char> caracteres;
+    for (char c : senha) {
+        if (caracteres.count(c) > 0) {
+            return false;
+        }
+        caracteres.insert(c);
+    }
+
+    // Se todas as condições forem atendidas, a senha é válida
+    return true;
+};
+
+std::string Senha::getSenha() const{
+    return m_senha;
+}
+
+void Senha::setSenha(std::string novaSenha){
+    if (validar(novaSenha)){
+        m_senha = novaSenha;
+    } else {
+        throw std::invalid_argument("Senha inválida");
+    }
+
+}
--- domains/Senha.hpp
+++ domains/Senha.hpp
@@ -2,8 +2,17 @@
 #define SENHA_HPP_INCLUDED
 
 #include "Validador.hpp"
+#include <string>
 
-class Senha : virtual public Validador {
+class Senha : virtual private Validador { // 222035625
+    private:
+        std::string m_senha;
+        bool validar(std::string senha);
+
+    public:
+        void setSenha(std::string novaSenha);
+        std::string getSenha() const;
+
 
 };
 
