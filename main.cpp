0e5d78c9bb0849716b483fd0200c12ece7b4d98c
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-09-29 17:48:23 -0300

    Reorganizacao dos testes novos

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
+// -*- coding: utf-8 -*-
 #include <iostream>
+#include <locale>
 
-#include "Quadro.hpp"
-#include "Conta.hpp"
-#include "domains/Senha.hpp"
-#include "domains/Codigo.hpp"
-#include "domains/Texto.hpp"
-#include "domains/Limite.hpp"
+#include "src/entities/Quadro.hpp"
+#include "src/entities/Cartao.hpp"
+#include "src/entities/Conta.hpp"
 
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL,"pt_BR.UTF-8");
+
     //Criacao de Conta
         cout << "---- Criando conta..." << endl;
         Conta c1(string("a.b@example.com"), string("Joao da Silva Jr"), string("xH?1nFm"));
-
         cout << "NOME: " << c1.getNome() << endl;
         cout << "EMAIL: " << c1.getEmail() << endl;
         cout << "SENHA: " << c1.getSenha() << endl;
+
 
         cout << "---- Alterando dados da conta..." << endl;
         c1.setNome("Daniel Melo");
@@ -28,39 +29,27 @@ int main()
         cout << "EMAIL: " << c1.getEmail() << endl;
         cout << "SENHA: " << c1.getSenha() << endl;
 
-        //Criação de quadro
+      //Criação de quadro
         cout << "---- Criando quadro..." << endl;
-        Codigo codigoObject("AA00");
-        Texto nomeObject("Meu quadro");
-        Texto descricaoObject("Minha descricao");
-        Limite limiteObject("5");
 
-        Quadro q1(codigoObject, nomeObject, descricaoObject, limiteObject);
+        Quadro q1(Codigo("AA00"), Texto("Meu quadro"),Texto("Minha descricao"), Limite("5"));
 
         cout << "CODIGO: " << q1.getCodigo() << endl;
         cout << "NOME: " << q1.getNome() << endl;
         cout << "DESCRICAO: " << q1.getDescricao() << endl;
         cout << "LIMITE: " << q1.getLimite() << endl;
-/*
 
-    // Criação de quadro
-    Quadro q1(5, "Meu quadro", "Minha descricao", 3);
-    cout << q1.getCodigo() << endl;
-    q1.setCodigo(6);
-    cout << q1.getCodigo() << endl;
-    cout << "-----------" << endl;
-    cout << q1.getNome() << endl;
-    q1.setNome("novo nome de quadro");
-    cout << q1.getNome() << endl;
-    cout << "-----------" << endl;
-    cout << q1.getDescricao() << endl;
-    q1.setDescricao("nova descricao");
-    cout << q1.getDescricao() << endl;
-    cout << "-----------" << endl;
-    cout << q1.getLimite() << endl;
-    q1.setLimite(4);
-    cout << q1.getLimite() << endl;
-*/
+        cout << "---- Alterando dados do quadro..." << endl;
+
+        q1.setNome("Meu quadrinho");
+        q1.setCodigo("BB00");
+        q1.setDescricao("L5sS,Lx");
+        q1.setLimite("10");
+        cout << "CODIGO: " << q1.getCodigo() << endl;
+        cout << "NOME: " << q1.getNome() << endl;
+        cout << "DESCRICAO: " << q1.getDescricao() << endl;
+        cout << "LIMITE: " << q1.getLimite() << endl;
+
     // Criação de carta
 /*    Carta c1(5, "Minha carta", "Minha descricao", 3);
     cout << c1.getCodigo() << endl;
