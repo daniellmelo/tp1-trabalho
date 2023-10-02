dc30cfe1432cc1139da673c5a7c7156546f5adeb
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-10-02 17:44:45 -0300

    adicionando comentários doxygen

--- src/entities/Conta.hpp
+++ src/entities/Conta.hpp
@@ -8,17 +8,23 @@
 
 class Conta { // 222035625
     public:
+    /// @brief Construtor da classe Conta.
+    ///
+    /// @param email O endereço de email associado à conta.
+    /// @param nome O nome do usuário associado à conta.
+    /// @param senha A senha da conta.
         Conta(Email email, Texto nome, Senha senha);
     private:
-        Email m_email;
-        Texto m_nome;
-        Senha m_senha;
+        Email m_email; ///< O endereço de email associado à conta.
+        Texto m_nome; ///< O nome do usuário associado à conta.
+        Senha m_senha; ///< A senha da conta.
     public:
         std::string getEmail() const;
         std::string getNome() const;
         std::string getSenha() const;
 
         void setEmail(std::string novoEmail);
+
         void setNome(std::string novoNome);
         void setSenha(std::string novoSenha);
 };
