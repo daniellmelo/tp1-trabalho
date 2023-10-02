9edc0040e1f15bf6ca71e61fea88c3bb4dfbdad8
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-10-02 18:21:26 -0300

    Adicionado documentacao

--- src/domains/Email.hpp
+++ src/domains/Email.hpp
@@ -4,14 +4,40 @@
 
 #include "Validador.hpp"
 
-class Email{ //190063882
-    public:
-        Email(std::string email);
-    public:
-        std::string m_email;
-    public:
-        void setEmail(std::string novoEmail);
-        std::string getEmail() const;
-     private:
-        void validar(std::string);
+/// @file Email.hpp
+/// @brief Este arquivo cont&eacute;m a declara&ccedil;&atilde;o da classe Email.
+///
+
+/// @class Email
+/// @brief Classe que representa um endere&ccedil;o de e-mail.
+///
+/// Esta classe representa um endere&ccedil;o de e-mail v&aacute;lido.
+/// Ela fornece m&eacute;todos para definir e obter o endere&ccedil;o de e-mail, bem como para validar sua formata&ccedil;&atilde;o.
+class Email { // 190063882
+public:
+    /// @brief Construtor da classe Email.
+    ///
+    /// @param email A sequ&ecirc;ncia de caracteres que representa o endere&ccedil;o de e-mail.
+    Email(std::string email);
+
+    /// @brief Define um novo endere&ccedil;o de e-mail.
+    ///
+    /// @param novoEmail O novo endere&ccedil;o de e-mail a ser definido.
+    void setEmail(std::string novoEmail);
+
+    /// @brief Obt&eacute;m o endere&ccedil;o de e-mail atual.
+    ///
+    /// @return Uma string representando o endere&ccedil;o de e-mail atual.
+    std::string getEmail() const;
+
+private:
+    /// @brief Fun&ccedil;&atilde;o privada para validar o endere&ccedil;o de e-mail.
+    ///
+    /// Esta fun&ccedil;&atilde;o &eacute; usada internamente para validar a formata&ccedil;&atilde;o do endere&ccedil;o de e-mail.
+    ///
+    /// @param email A sequ&ecirc;ncia de caracteres a ser validada como endere&ccedil;o de e-mail.
+    void validar(std::string email);
+
+private:
+    std::string m_email; ///< O endere&ccedil;o de e-mail.
 };
--- src/domains/Limite.hpp
+++ src/domains/Limite.hpp
@@ -2,12 +2,39 @@
 
 #include "Validador.hpp"
 
-class Limite{
-    private:
-        std::string m_limite;
-        void validar(std::string limite);
-    public:
-        void setLimite(std::string novoLimite);
-        std::string getLimite() const;
-        Limite(std::string limite);
+/// @file Limite.hpp
+/// @brief Este arquivo cont&eacute;m a declara&ccedil;&atilde;o da classe Limite.
+///
+
+/// @class Limite
+/// @brief Classe que representa um limite.
+///
+/// Esta classe representa um limite, que &eacute; uma sequ&ecirc;ncia de caracteres.
+/// Ela fornece m&eacute;todos para definir e obter o limite, bem como para validar sua formata&ccedil;&atilde;o.
+class Limite {
+private:
+    std::string m_limite; ///< O limite.
+
+    /// @brief Fun&ccedil;&atilde;o privada para validar o limite.
+    ///
+    /// Esta fun&ccedil;&atilde;o &eacute; usada internamente para validar a formata&ccedil;&atilde;o do limite.
+    ///
+    /// @param limite A sequ&ecirc;ncia de caracteres a ser validada como limite.
+    void validar(std::string limite);
+
+public:
+    /// @brief Construtor da classe Limite.
+    ///
+    /// @param limite A sequ&ecirc;ncia de caracteres que representa o limite.
+    Limite(std::string limite);
+
+    /// @brief Define um novo limite.
+    ///
+    /// @param novoLimite A nova sequ&ecirc;ncia de caracteres a ser definida como limite.
+    void setLimite(std::string novoLimite);
+
+    /// @brief Obt&eacute;m o limite atual.
+    ///
+    /// @return Uma string representando o limite atual.
+    std::string getLimite() const;
 };
