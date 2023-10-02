99ea688a6d356667c6dd5c0696f486aec39234d4
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-10-02 18:28:12 -0300

    Adicionando documentacao

--- src/domains/Coluna.hpp
+++ src/domains/Coluna.hpp
@@ -2,11 +2,34 @@
 
 #include "Validador.hpp"
 
+/// @file Coluna.hpp
+/// @brief Este arquivo cont&eacute;m a declara&ccedil;&atilde;o da classe Coluna.
+///
+
+/// @class Coluna
+/// @brief Classe que representa uma coluna.
+///
+/// Esta classe representa uma coluna, que pode ser validada de acordo com algum crit&eacute;rio definido.
 class Coluna : virtual public Validador {
-    private:
-        std::string coluna;
-        bool validar(std::string);
-    public:
-        void setColuna(std::string);
-        std::string getColuna() const;
+private:
+    std::string coluna; ///< A coluna.
+
+    /// @brief Fun&ccedil;&atilde;o privada para validar a coluna.
+    ///
+    /// Esta fun&ccedil;&atilde;o &eacute; usada internamente para validar a coluna de acordo com algum crit&eacute;rio.
+    ///
+    /// @param coluna A sequ&ecirc;ncia de caracteres a ser validada como coluna.
+    /// @return true se a coluna for v&aacute;lida, false caso contr&aacute;rio.
+    bool validar(std::string coluna);
+
+public:
+    /// @brief Define uma nova coluna.
+    ///
+    /// @param novaColuna A nova sequ&ecirc;ncia de caracteres a ser definida como coluna.
+    void setColuna(std::string novaColuna);
+
+    /// @brief Obt&eacute;m a coluna atual.
+    ///
+    /// @return Uma string representando a coluna atual.
+    std::string getColuna() const;
 };
