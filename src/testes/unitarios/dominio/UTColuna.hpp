23654c2f02550c001a3833731f25e10d35d47477
Daniel Melo <92686481+daniellmelo@users.noreply.github.com>
2023-10-11 15:28:22 -0300

    Definicao da classe de testes do Dominio

---
+++ src/testes/unitarios/dominio/UTColuna.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "../../../domains/Coluna.hpp"
+
+class UTColuna {
+private:
+    const static std::string VALOR_VALIDO;   // Defini??????o de constante para evitar n???mero m???gico.
+    const static std::string VALOR_INVALIDO;   // Defini??????o de constante para evitar n???mero m???gico.
+    Coluna *coluna;                         // Refer???ncia para unidade em teste.
+    int estado;                             // Estado do teste.
+    void setUp();                           // M???todo para criar unidade em teste.
+    void tearDown();                        // M???todo para destruir unidade em teste.
+    void testarCenarioSucesso();            // Cen???rio de teste.
+    void testarCenarioFalha();              // Cen???rio de teste.
+
+public:
+    const static int SUCESSO =  0;          // Defini??????o de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // Defini??????o de constante para reportar resultado de teste.
+    int run();                              // M???todo para executar teste.
+};
+
